107c284fec6ddf9e79f6dd7cdaa1c3a533b05f9f
coke <2639555042@qq.com>
2026-08-01 22:36:33 +0800

    build: 添加vercel适配器

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
-  site: 'https://fartmonarch.xyz',   
+  site: 'https://fartmonarch.xyz',
   integrations: [mdx(), icon()],
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
     },
   },
+
   i18n: {
     defaultLocale: "zh",           
     locales: ["zh", "en"],         
     routing: {
       prefixDefaultLocale: false   
     }
-  }
+  },
+
+  adapter: vercel()
 });
